ab546a36d3317539c1603aa2d2d518dc2620ed67
Karn <mail@karngyan.com>
2020-08-07 21:21:58 +0530

    init oneschool, front: wip

---
+++ README.md
@@ -0,0 +1,10 @@
+To run a local server, execute this
+command:
+
+`mvn package appengine:run`
+
+### Screenshots
+
+<p align="center">
+    <img alt="Screenshot 3" src="../../assets/screenshot_0.png?raw=true"/>
+</p>
---
+++ src/main/webapp/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      blue: '#6C63FF',
+      lightblue: '#F1F0FF',
+      darkgrey: '#444053',
+      lightgrey: '#AEAEAE',
+      red: '#de3618',
+      indigo: '#5c6ac4',
+      white: '#ffffff'
+    },
+    fontFamily: {
+      'body': ['Fira Code'],
+    },
+    extend: {},
+  },
+  variants: {
+    outline: ["focus"],
+  },
+  plugins: [],
+  corePlugins: {
+    outline: false,
+  }
+}
