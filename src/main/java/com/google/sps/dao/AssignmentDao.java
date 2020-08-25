79dc2926fc3ea6ad5a7e9953f1949e266f5e92ed
17tanya <tanya.ravindra@gmail.com>
2020-08-26 03:44:14 +0530

    Add doPut method

--- src/main/java/com/google/sps/dao/AssignmentDao.java
+++ src/main/java/com/google/sps/dao/AssignmentDao.java
@@ -7,6 +7,7 @@ import com.google.firebase.cloud.FirestoreClient;
 import com.google.sps.models.*;
 import com.google.sps.utils.validation.ValidationErrors;
 import com.google.sps.utils.validation.ValidationResponse;
+import com.google.sps.utils.validation.ValidationUtils;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,6 +36,10 @@ public class AssignmentDao implements  IAssignmentDao {
         List<ClassroomStudent> students = new ClassroomStudentDao().getStudentsForClassroom(assignment.getClassroomId());
         Classroom classroom = new ClassroomDao().getClassroomById(assignment.getClassroomId());
         assignment.setEducatorId(classroom.getEducatorId());
+
+        String assignmentId = UUID.randomUUID().toString();
+        assignment.setAssignmentId(id);
+
         for (ClassroomStudent classroomStudent: students) {
             String id = UUID.randomUUID().toString();
             assignment.setId(id);
@@ -88,5 +93,136 @@ public class AssignmentDao implements  IAssignmentDao {
             assignments.add(assignment);
         }
         return assignments;
+    }
+
+    public ValidationResponse updateAssignmentForStudent(Assignment newAssignment) {
+        //Set attribute submitted to true
+
+        ValidationResponse.Builder validationResponseBuilder = ValidationResponse.builder();
+        validationResponseBuilder.status(ValidationErrors.STATUS_OK);
+        validationResponseBuilder.message(ValidationErrors.MESSAGE_OK);
+
+        Assignment oldAssignment = getAssignmentById(newAssignment.getId());
+        newAssignment.setId(oldAssignment.getId());
+        newAssignment.setUpdated(System.currentTimeMillis());
+
+        // weird not null is zero but
+        if (newAssignment.getCreated() == 0) {
+            newAssignment.setCreated(oldAssignment.getCreated());
+        }
+
+        if (newAssignment.getAssignmentId() == 0) {
+            newAssignment.setAssignmentId(oldAssignment.getAssignmnetId());
+        }
+
+        if (newAssignment.getClassroomId() == 0) {
+            newAssignment.setClassroomId(oldAssignment.getClassroomId());
+        }
+
+        if (newAssignment.getEducatorId() == 0) {
+            newAssignment.setEducatorId(oldAssignment.getEducatorId());
+        }
+
+        if (newAssignment.getDeadline() == 0) {
+            newAssignment.setDeadline(oldAssignment.getDeadline());
+        }
+
+        if (newAssignment.getTotal_marks() == 0) {
+            newAssignment.setTotal_marks(oldAssignment.getTotal_marks());
+        }
+
+        if (newAssignment.getName() == null) {
+            newAssignment.setName(oldAssignment.getName());
+        }
+
+        if (newAssignment.getDescription() == null) {
+            newAssignment.setDescription(oldAssignment.getDescription());
+        }
+
+        newAssignment.setSubmitted(true));
+
+        ApiFuture<WriteResult> writeResult = db.collection(Assignment.Keys.COLLECTION)
+                .document(newAccount.getId()).set(newAssignment);
+
+        log.info("Update assignment called: " + writeResult.get().toString());
+
+        return validationResponse;
+
+    }
+
+    public ValidationResponse updateAssignmentForEducator(Assignment newAssignment) {
+        //Set attribute checked to true
+
+        ValidationResponse.Builder validationResponseBuilder = ValidationResponse.builder();
+        validationResponseBuilder.status(ValidationErrors.STATUS_OK);
+        validationResponseBuilder.message(ValidationErrors.MESSAGE_OK);
+
+        Assignment oldAssignment = getAssignmentById(newAssignment.getId());
+        newAssignment.setId(oldAssignment.getId());
+        newAssignment.setUpdated(System.currentTimeMillis());
+
+        // weird not null is zero but
+        if (newAssignment.getCreated() == 0) {
+            newAssignment.setCreated(oldAssignment.getCreated());
+        }
+
+        if (newAssignment.getAssignmentId() == 0) {
+            newAssignment.setAssignmentId(oldAssignment.getAssignmnetId());
+        }
+
+        if (newAssignment.getClassroomId() == 0) {
+            newAssignment.setClassroomId(oldAssignment.getClassroomId());
+        }
+
+        if (newAssignment.getEducatorId() == 0) {
+            newAssignment.setEducatorId(oldAssignment.getEducatorId());
+        }
+
+        if (newAssignment.getDeadline() == 0) {
+            newAssignment.setDeadline(oldAssignment.getDeadline());
+        }
+
+        if (newAssignment.getTotal_marks() == 0) {
+            newAssignment.setTotal_marks(oldAssignment.getTotal_marks());
+        }
+
+        if (newAssignment.getName() == null) {
+            newAssignment.setName(oldAssignment.getName());
+        }
+
+        if (newAssignment.getDescription() == null) {
+            newAssignment.setDescription(oldAssignment.getDescription());
+        }
+
+        if (!newAssignment.isSubmitted()) {
+            newAssignment.setSubmitted(oldAssignment.isSubmitted());
+        }
+
+        newAssignment.setChecked(true);
+
+        ApiFuture<WriteResult> writeResult = db.collection(Assignment.Keys.COLLECTION)
+                .document(newAccount.getId()).set(newAssignment);
+
+        log.info("Update assignment called: " + writeResult.get().toString());
+
+        return validationResponse;
+    }
+
+    @Override
+    @SneakyThrows
+    public Assignment getAssignmentById(String id) {
+        DocumentReference documentReference = db.collection(Assignment.Keys.COLLECTION)
+                .document(id);
+
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot documentSnapshot = future.get();
+
+        if (documentSnapshot.exists()) {
+            Assignment assignment = documentSnapshot.toObject(Assignment.class);
+            assignment.setId(documentSnapshot.getId());
+            return classroom;
+        }
+        return null;
     }
 }
--- src/main/java/com/google/sps/dao/IAssignmentDao.java
+++ src/main/java/com/google/sps/dao/IAssignmentDao.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface IAssignmentDao {
     ValidationResponse createAssignment(Assignment assignment);
+    Assignment getAssignmentById(String id);
     List<Assignment> getAllAssignmentsStudent(String studentId);
     List<Assignment> getAllAssignmentsEducator(String educatorId);
 //    List<Assignment> getAllPendingAssignments();
