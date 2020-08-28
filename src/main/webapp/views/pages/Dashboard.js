e648eed96c9a4753633fb530c8892e37fbba2291
Karn <mail@karngyan.com>
2020-08-28 07:18:36 +0530

    remove your profile from dropdown

--- src/main/webapp/views/pages/Dashboard.js
+++ src/main/webapp/views/pages/Dashboard.js
@@ -182,8 +182,7 @@ const Educator = {
                       class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
                       <div class="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                         <!-- Add more options like profile, settings -->
-                        <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Your Profile</a>
-                        <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Settings</a>
+                        <!-- <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Your Profile</a> -->
                         <a href="#" id="sign-out-btn" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Sign out</a>
                       </div>
                     </div>
